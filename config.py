cc3e87bb5d80a899a72c8284b8250f5740d4901b
Keethesh <keethesh15@Gmail.com>
2021-10-28 20:16:22 +0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,1 @@
+bot_token = 'OTAzMzA3MjMyODI4MDg4MzQw.YXrEag.MSQCcJcqB2tywUyYVIcUceSLYIE'
---
+++ main.py
@@ -0,0 +1,37 @@
+from aiohttp import ClientSession
+from discord import Embed
+from discord.ext import commands
+from discord.ext.commands import Context
+from lxml import html
+
+client = commands.Bot(command_prefix='!')
+fact_cache = {'expires': 0, 'facts': []}
+
+
+@client.event
+async def on_ready():
+    print(f"Logged on as {client.user}!")
+
+
+async def get_fact():
+    async with ClientSession() as session:
+        async with session.get('https://chucknorrisfacts.fr/facts/random') as response:
+            response = await response.read()
+
+    doc = html.fromstring(response)
+    facts = doc.xpath("//p[@class='card-text']")
+    return facts[0].text
+
+
+@client.command()
+async def chuck(ctx: Context):
+    fact = await get_fact()
+    embed = Embed(title="Chuck Fact", description=fact, color=0x0440b9)
+    await ctx.send(embed=embed)
+
+
+if __name__ == '__main__':
+    import config
+
+    token = config.bot_token
+    client.run(token)
